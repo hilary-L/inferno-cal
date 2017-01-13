71733052b079ad833a50b9460623d689fc1b47c9
Hilary Lee <hilary@digintent.com>
2017-01-13 14:40:15 -0600

    Initial commit.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import Inferno from 'inferno'
 
 import App from './App'
 
+if(module.hot) {
+	require('inferno-devtools')
+}
+
 Inferno.render(<App/>, document.querySelector('#app'))
 
 if (module.hot) {
